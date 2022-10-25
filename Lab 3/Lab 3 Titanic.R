771b4322809abe9c1467a5f1a6b60fd61da855cf
sindhoora8 <sindhooramandadi20@gmail.com>
2022-10-25 15:38:15 -0400

    Update Lab 3 Titanic.R

--- Lab 3/Lab 3 Titanic.R
+++ Lab 3/Lab 3 Titanic.R
@@ -1,6 +1,7 @@
 install.packages("titanic")
 data(Titanic)
-titanic <- tree(Survived ~ ., data=Titanic)
+library(rpart)
+titanic <- rpart(Survived ~ ., data=Titanic)
 titanic
 titanic$frame
 plot(titanic)
